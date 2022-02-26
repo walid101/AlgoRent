43e04185e827bfe28142398e864d2bbeff722c6b
walid101 <nuhankhan101@gmail.com>
2022-02-26 14:46:32 -0500

    Fixed User Facing Bugs

--- AlgoRent/views.py
+++ AlgoRent/views.py
@@ -1,4 +1,5 @@
 from audioop import add
+from random import randrange
 from django.shortcuts import render
 import sys
 import os
@@ -32,5 +33,7 @@ async def home(request):
         except Exception as err:
             #error supression
             print(err)
+    for info_s in info:
+        info_s["price"] = randrange(85000, 800000)
     return render(request, 'home.html', context={"house_info_a": info[0:half], "house_info_b": info[half:2*half],
                                                  "house_info_c": info[2*half:]})
